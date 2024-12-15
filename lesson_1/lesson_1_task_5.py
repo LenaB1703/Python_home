0b579b54802b65877b4071dfcd37949a7ad65e8b
LenaB1703 <lena.bolshackowa@mail.ru>
2024-12-15 17:26:42 +0300

    My second commit

---
+++ lesson_1/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+#Параметризация функции
+def print_namber(num):
+    print(num, end='')
+
+print_namber(8)
+print_namber(8)
+print_namber(0)
+print_namber(0)
+print_namber(5)
+print_namber(5)
+print_namber(5)
+print_namber(3)
+print_namber(5)
+print_namber(3)
+print_namber(5)
